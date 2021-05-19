15ac019dd776350baaf02fa52ded54f448f3ed41
Runetotem <gabriel.pkmn95@gmail.com>
2021-05-19 01:37:29 -0300

    Add redux to product screen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,33 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { detailsProduct } from '../actions/productActions';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 import Rating from '../components/Rating';
-import data from '../data';
 
 export default function ProductScreen(props) {
-  const product = data.products.find((x) => x._id === props.match.params.id);
-  if (!product) {
-    return <div> Product Not Found</div>;
-  }
-  return ( 
+  const dispatch = useDispatch();
+  const productId = props.match.params.id;
+  const productDetails = useSelector((state) => state.productDetails);
+  const { loading, error, product } = productDetails;
+   
+  useEffect(() => {
+    dispatch(detailsProduct(productId));
+  }, [dispatch, productId]);
+
+  return (
     <div>
-      <Link to="/">Back to result</Link>
-      <div className="row top">
-        <div className="col-2">
-          <img className="large" src={product.image} alt={product.name}></img>
-        </div>
-        <div className="col-1">
-          <ul>
-            <li>
-              <h1>{product.name}</h1>
-            </li>
-            <li>
-              <Rating 
-                rating={product.rating} 
-                numReviews={product.numReviews}
-              ></Rating>
-            </li>
-            <li>Price : ${product.price}</li>
-            <li>
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <div>
+          <Link to="/">Back to result</Link>
+          <div className="row top">
+            <div className="col-2">
+              <img 
+                className="large" 
+                src={product.image} 
+                alt={product.name}
+              ></img>
+          </div>
+          <div className="col-1">
+            <ul>
+              <li>
+                <h1>{product.name}</h1>
+              </li>
+              <li>
+                <Rating 
+                  rating={product.rating} 
+                  numReviews={product.numReviews}
+                ></Rating>
+              </li>
+              <li>Price : ${product.price}</li>
+              <li>
               Description:
               <p>{product.description}</p>
             </li>
@@ -62,5 +80,8 @@ export default function ProductScreen(props) {
         </div>
       </div>
     </div>
+      )}
+    </div> 
+    
   );
 }
