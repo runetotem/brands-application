407c152a0630f307b8c44302560c4bff8f9c54ab
Runetotem <gabriel.pkmn95@gmail.com>
2021-05-26 01:43:48 -0300

    Creating sample users in MongoDB

--- backend/data.js
+++ backend/data.js
@@ -1,5 +1,22 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
-  products:[
+  users: [
+    {
+      name:'Luke',
+      email:'admin@example.com',
+      password: bcrypt.hashSync('1234', 8),
+      isAdmin: true,
+    },
+    {
+      name:'Jmbreal',
+      email:'user@example.com',
+      password: bcrypt.hashSync('1234', 8),
+      isAdmin: false,
+    },
+
+  ],
+  products: [
     {
       _id: '1',
       name: 'Nike Slim Shirt',
