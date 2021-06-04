c272cbaf25989ed9565a15846079a4ada5c7adfe
Runetotem <gabriel.pkmn95@gmail.com>
2021-06-04 17:26:24 -0300

    place order action

--- backend/utils.js
+++ backend/utils.js
@@ -16,7 +16,7 @@ export const generateToken = (user) => {
 };
 
 export const isAuth = (req, res, next) => {
-  const authorization = req.header.authorization;
+  const authorization = req.headers.authorization;
   if (authorization) {
     const token = authorization.slice(7, authorization.length); // Bearer XXXXXX
     jwt.verify(
--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,15 +1,21 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_PAYMENT_METHOD, CART_SAVE_SHIPPING_ADDRESS } from "../constants/cartConstants";
+import { 
+  CART_ADD_ITEM, 
+  CART_EMPTY, 
+  CART_REMOVE_ITEM, 
+  CART_SAVE_PAYMENT_METHOD, 
+  CART_SAVE_SHIPPING_ADDRESS, 
+} from "../constants/cartConstants";
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
   switch(action.type) {
     case CART_ADD_ITEM:
       const item = action.payload;
       const existItem = state.cartItems.find((x) => x.product === item.product);
-      if(existItem){
+      if (existItem) {
         return {
           ...state,
           cartItems: state.cartItems.map((x) =>
-            x.product === existItem.product ? item: x
+            x.product === existItem.product ? item : x
           ),
         };
       } else {
@@ -24,6 +30,8 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
     return { ...state, shippingAddress: action.payload };
   case CART_SAVE_PAYMENT_METHOD:
     return {...state, paymentMethod: action.payload };
+  case CART_EMPTY:
+    return { ...state, cartItems: [] };
   default: 
     return state;
   }
